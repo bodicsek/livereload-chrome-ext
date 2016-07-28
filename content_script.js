320b340c4fa6a18cc471a39d1fe6a1ecd9e104df
David Nabraczky <david.nabraczky@gmail.com>
2016-07-28 19:58:25 +0200

    Injecting script tag with src attribute

--- content_script.js
+++ content_script.js
@@ -8,13 +8,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 
 function injectLivereload() {
     var script = document.createElement("script");
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://raw.githubusercontent.com/livereload/livereload-js/master/dist/livereload.js", true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            script.text = xhr.responseText;
-            document.body.appendChild(script);
-        }
-    }
-    xhr.send();
+    script.setAttribute("src",
+        "https://raw.githubusercontent.com/livereload/livereload-js/master/dist/livereload.js");
+    document.head.appendChild(script);
 }
